be15b2f93569a8a065bd442e7a89c992a5085905
unknown <jspacific777@gmail.com>
2022-08-07 15:13:01 -1200

    proper style added

--- script.js
+++ script.js
@@ -1,61 +1,48 @@
-let output = document.getElementById('output');
+let output = document.querySelector('.screen');
 const btns = document.querySelectorAll('.btn');
 let equiv = document.querySelector('.equiv');
-
-
-
+let del = document.querySelector('.del');
+const operators = document.querySelectorAll('.opr');
 
 function calc(num1, num2, operator) {
    if (operator === '-')
       ans = num1 - num2;
    if (operator === '+')
       ans = num1 + num2
-   if (operator === '*')
+   if (operator === 'x')
       ans = num1 * num2
    if (operator === '^')
       ans = num1 ** num2
    if (operator === '/')
       ans = num1 / num2
-
-   return ans
+   return ans;
 }
+// console.log(calc(3, 3, '^'));
 
-equiv.addEventListener('click', () => {
-   let equation = output.innerText
-   let neweq = eval(equation)
-   output.innerText = ''
-   output.innerText = neweq;
-})
-
-// console.log(calc(6, 4, '^'));
 
 btns.forEach(btn => {
    btn.addEventListener('click', (e) => {
-      myValue = e.target.innerText
-      output.innerText += myValue
-   })
+      myValue = e.target.innerText;
+      output.innerText += myValue;
 
+   })
+});
+
+equiv.addEventListener('click', () => {
+   let eqn = output.innerText;
+   let neweqn = eval(eqn);
+   output.innerText = neweqn;
+
+   // neweqn == eval(eqn) ? equiv : '';
+   output.focus();
 });
 
 
-// let equiv =
 
-
-/*
-input.addEventListener('change', () => {
-   let num1 = parseFloat(input.value);
-   input.value = `${num1} ${operator} ${num2} calc(num1, operator, num2)`
-
-
-   input.focus();
-
+del.addEventListener('click', () => {
+   output.innerText = '';
 })
-*/
 
 
-// function cookie(string) {
-//    var cookies = "cookie:" + string;
-//    return cookies;
-// }
-// console.log("what is your favorite cookies?");
-// console.log("peanuts");
+
+
